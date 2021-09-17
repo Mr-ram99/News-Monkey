72880c1f5b28b7be25ca4a2864d40b786b6b589e
Mr-ram9 <jrchoudhary2410@gmail.com>
2021-09-17 13:44:54 +0530

    Changed card look and added Prev and Next btn

--- src/Components/News.js
+++ src/Components/News.js
@@ -7,19 +7,51 @@ export class News extends Component {
     constructor() {
         super();
         this.state = {
-            // apikey : "ced035a95bc948339fe954824bfbb443",
+            apikey: "ced035a95bc948339fe954824bfbb443",
             articles: [],
-            loading: false
+            loading: false,
+            page: 1
         }
     }
-    async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=ced035a95bc948339fe954824bfbb443";
+    async componentDidMount() {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${this.state.apikey}&page=${this.state.page}&pageSize=20`;
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
         this.setState({
+            articles: parsedData.articles,
+            totalArticles: parsedData.totalResults
+        })
+    }
+
+    handlePreviousClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${this.state.apikey}&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+            page: this.state.page - 1
+        })
+        this.setState({
             articles: parsedData.articles
         })
+    }
+    handleNextClick = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalArticles / 20)) { 
+
+        }
+        else {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${this.state.apikey}&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            console.log(parsedData);
+            this.setState({
+                page: this.state.page + 1
+            })
+            this.setState({
+                articles: parsedData.articles
+            })
+        }
     }
     render() {
         return (
@@ -27,10 +59,14 @@ export class News extends Component {
                 <h2>NewsMonkey - Top Headlines</h2>
                 <div className="row">
                     {this.state.articles.map((element) => {
-                        return <div className="col-md-4 my-2 " key={element.url}>
-                                    <NewsItem title={element.title} desc={element.description} imageUrl={element.urlToImage?element.urlToImage:"https://newsinterpretation.com/wp-content/uploads/2020/03/news33.jpg"} newsUrl={element.url}/>
-                                </div>
+                        return <div key={element.url}>
+                            <NewsItem title={element.title} desc={element.description} imageUrl={element.urlToImage ? element.urlToImage : "https://newsinterpretation.com/wp-content/uploads/2020/03/news33.jpg"} newsUrl={element.url} />
+                        </div>
                     })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button type="button" className="btn btn-dark" onClick={this.handlePreviousClick} disabled={this.state.page <= 1}>&larr; Previous</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick} disabled={this.state.page + 1 > Math.ceil(this.state.totalArticles / 20)}>Next &rarr;</button>
                 </div>
             </div>
         )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
- 
+
     render() {
-        const {title, desc, imageUrl, newsUrl} = this.props;
+        const { title, desc, imageUrl, newsUrl } = this.props;
         return (
-            <div className="card" style={{width: "20rem"}}>
-                <img src={imageUrl} className="card-img-top" alt="news-img" />
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{desc}</p>
-                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More &rarr;</a>
+            <div className="card mb-3" style={{ maxWidth: "60rem" }}>
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <img src={imageUrl} className="img-fluid rounded-start" alt="news-img" />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{title}</h5>
+                            <p className="card-text">{desc}</p>
+                            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More &rarr;</a>
+                        </div>
+                    </div>
                 </div>
             </div>
+
 
         )
     }
